db82afa882d1e1d318d727403b57334b189e01d0
Phillip Kast <kastph@gmail.com>
2012-06-19 13:19:08 -0700

    [NSString stringWithString:@...] // Wat?

--- Classes/ELCAlbumPickerController.m
+++ Classes/ELCAlbumPickerController.m
@@ -86,13 +86,13 @@ static int compareGroupsUsingSelector(id p1, id p2, void *context)
                              // If we encounter a location services error, prompt the user to enable location services
                              if ([error code] == ALAssetsLibraryAccessUserDeniedError) {
                                  errorMessage = [NSString stringWithFormat:@"It looks like you've disabled location services for this app. To add photos, enable \"Location Services\" for %@ in your device's \"Settings\" App.",[[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleName"]];
-                                 errorTitle = [NSString stringWithString:@"Oops!"];
+                                 errorTitle = @"Oops!";
                              } else if ([error code] == ALAssetsLibraryAccessGloballyDeniedError) {
-                                 errorMessage = [NSString stringWithString:@"It looks like you've disabled location services on your device. To add photos, enable \"Location Services\" in your device's \"Settings\" App."]; 
-                                 errorTitle = [NSString stringWithString:@"Oops!"];
+                                 errorMessage = @"It looks like you've disabled location services on your device. To add photos, enable \"Location Services\" in your device's \"Settings\" App.";
+                                 errorTitle = @"Oops!";
                              } else {
                                  errorMessage = [NSString stringWithFormat:@"Album Error: %@", [error localizedDescription]];
-                                 errorTitle = [NSString stringWithString:@"Error"];
+                                 errorTitle = @"Error";
                              }
                              
                              UIAlertView * alert = [[UIAlertView alloc] initWithTitle:errorTitle
